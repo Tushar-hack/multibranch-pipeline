226705632e2b4ef1106a55eff806717d6beae032
Ｔｕｓｈａｒ　Ｔａｋ <73552293+Tushar-hack@users.noreply.github.com>
2023-03-18 17:06:27 +0530

    Create script.groovy

---
+++ script.groovy
@@ -0,0 +1,11 @@
+def buildApp(){
+    echo "This is a Build stage...."
+}
+
+def testApp(){
+    echo "This is a Test stage...."
+}
+def deployApp(){
+    echo "Deploying an app to Production Server..."
+}
+return this
